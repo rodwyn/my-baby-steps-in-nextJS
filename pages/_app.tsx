b11bf73af0d8c46a9265622c2d616966be43eadb
Rodwyn Moreno <rodwyn.moreno@globant.com>
2023-05-04 17:09:36 -0600

    Added changes.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,11 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import type { Page } from "../types/page";
 
-type Props = AppProps & {
+type NextPageWithLayout = AppProps & {
   Component: Page;
 };
 
-export default function App({ Component, pageProps }: Props) {
+export default function App({ Component, pageProps }: NextPageWithLayout) {
   const getLayout = Component.getLayout || ((page) => page);
   return getLayout(<Component {...pageProps} />);
 }
